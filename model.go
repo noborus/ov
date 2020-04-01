d86574d419f0f1058c14fcbb9a8c275d4ff7da2e
Noboru Saito <noborusai@gmail.com>
2020-04-01 18:16:19 +0900

    Ignore move escape sequences
    "Grep --color" becomes normal by explicitly ignoring.

--- model.go
+++ model.go
@@ -131,81 +131,79 @@ func lookupColor(colorNumber int, bright bool) string {
 	}[colorNumber]
 }
 
-func csToStyle(csiParameter *bytes.Buffer, runeValue rune) (*bytes.Buffer, tcell.Style) {
+func csToStyle(csiParameter *bytes.Buffer, runeValue rune) tcell.Style {
 	style := tcell.StyleDefault
-	if runeValue == 'm' {
-		fields := strings.Split(csiParameter.String(), ";")
-		if len(fields) == 0 || len(fields) == 1 && fields[0] == "0" {
+	fields := strings.Split(csiParameter.String(), ";")
+	if len(fields) == 0 || len(fields) == 1 && fields[0] == "0" {
+		style = style.Normal()
+	}
+FieldLoop:
+	for index, field := range fields {
+		switch field {
+		case "1", "01":
+			style = style.Bold(true)
+		case "2", "02":
+			style = style.Dim(true)
+		case "4", "04":
+			style = style.Underline(true)
+		case "5", "05":
+			style = style.Blink(true)
+		case "7", "07":
+			style = style.Reverse(true)
+		case "22", "24", "25", "27":
 			style = style.Normal()
-		}
-	FieldLoop:
-		for index, field := range fields {
-			switch field {
-			case "1", "01":
-				style = style.Bold(true)
-			case "2", "02":
-				style = style.Dim(true)
-			case "4", "04":
-				style = style.Underline(true)
-			case "5", "05":
-				style = style.Blink(true)
-			case "7", "07":
-				style = style.Reverse(true)
-			case "22", "24", "25", "27":
-				style = style.Normal()
-			case "30", "31", "32", "33", "34", "35", "36", "37":
-				colorNumber, _ := strconv.Atoi(field)
-				style = style.Foreground(tcell.GetColor(lookupColor(colorNumber-30, false)))
-			case "39":
-				style = style.Foreground(tcell.ColorDefault)
-			case "40", "41", "42", "43", "44", "45", "46", "47":
-				colorNumber, _ := strconv.Atoi(field)
-				style = style.Background(tcell.GetColor(lookupColor(colorNumber-40, false)))
-			case "49":
-				style = style.Background(tcell.ColorDefault)
-			case "90", "91", "92", "93", "94", "95", "96", "97":
-				colorNumber, _ := strconv.Atoi(field)
-				style = style.Foreground(tcell.GetColor(lookupColor(colorNumber-90, true)))
-			case "100", "101", "102", "103", "104", "105", "106", "107":
-				colorNumber, _ := strconv.Atoi(field)
-				style = style.Background(tcell.GetColor(lookupColor(colorNumber-100, true)))
-			case "38", "48":
-				var color string
-				if len(fields) > index+1 {
-					if fields[index+1] == "5" && len(fields) > index+2 { // 8-bit colors.
-						colorNumber, _ := strconv.Atoi(fields[index+2])
-						if colorNumber <= 7 {
-							color = lookupColor(colorNumber, false)
-						} else if colorNumber <= 15 {
-							color = lookupColor(colorNumber, true)
-						} else if colorNumber <= 231 {
-							red := (colorNumber - 16) / 36
-							green := ((colorNumber - 16) / 6) % 6
-							blue := (colorNumber - 16) % 6
-							color = fmt.Sprintf("#%02x%02x%02x", 255*red/5, 255*green/5, 255*blue/5)
-						} else if colorNumber <= 255 {
-							grey := 255 * (colorNumber - 232) / 23
-							color = fmt.Sprintf("#%02x%02x%02x", grey, grey, grey)
-						}
-					} else if fields[index+1] == "2" && len(fields) > index+4 { // 24-bit colors.
-						red, _ := strconv.Atoi(fields[index+2])
-						green, _ := strconv.Atoi(fields[index+3])
-						blue, _ := strconv.Atoi(fields[index+4])
-						color = fmt.Sprintf("#%02x%02x%02x", red, green, blue)
+		case "30", "31", "32", "33", "34", "35", "36", "37":
+			colorNumber, _ := strconv.Atoi(field)
+			style = style.Foreground(tcell.GetColor(lookupColor(colorNumber-30, false)))
+		case "39":
+			style = style.Foreground(tcell.ColorDefault)
+		case "40", "41", "42", "43", "44", "45", "46", "47":
+			colorNumber, _ := strconv.Atoi(field)
+			style = style.Background(tcell.GetColor(lookupColor(colorNumber-40, false)))
+		case "49":
+			style = style.Background(tcell.ColorDefault)
+		case "90", "91", "92", "93", "94", "95", "96", "97":
+			colorNumber, _ := strconv.Atoi(field)
+			style = style.Foreground(tcell.GetColor(lookupColor(colorNumber-90, true)))
+		case "100", "101", "102", "103", "104", "105", "106", "107":
+			colorNumber, _ := strconv.Atoi(field)
+			style = style.Background(tcell.GetColor(lookupColor(colorNumber-100, true)))
+		case "38", "48":
+			var color string
+			if len(fields) > index+1 {
+				if fields[index+1] == "5" && len(fields) > index+2 { // 8-bit colors.
+					colorNumber, _ := strconv.Atoi(fields[index+2])
+					if colorNumber <= 7 {
+						color = lookupColor(colorNumber, false)
+					} else if colorNumber <= 15 {
+						color = lookupColor(colorNumber, true)
+					} else if colorNumber <= 231 {
+						red := (colorNumber - 16) / 36
+						green := ((colorNumber - 16) / 6) % 6
+						blue := (colorNumber - 16) % 6
+						color = fmt.Sprintf("#%02x%02x%02x", 255*red/5, 255*green/5, 255*blue/5)
+					} else if colorNumber <= 255 {
+						grey := 255 * (colorNumber - 232) / 23
+						color = fmt.Sprintf("#%02x%02x%02x", grey, grey, grey)
 					}
+				} else if fields[index+1] == "2" && len(fields) > index+4 { // 24-bit colors.
+					red, _ := strconv.Atoi(fields[index+2])
+					green, _ := strconv.Atoi(fields[index+3])
+					blue, _ := strconv.Atoi(fields[index+4])
+					color = fmt.Sprintf("#%02x%02x%02x", red, green, blue)
 				}
-				if len(color) > 0 {
-					if field == "38" {
-						style = style.Foreground(tcell.GetColor(color))
-					} else {
-						style = style.Background(tcell.GetColor(color))
-					}
-					break FieldLoop
+			}
+			if len(color) > 0 {
+				if field == "38" {
+					style = style.Foreground(tcell.GetColor(color))
+				} else {
+					style = style.Background(tcell.GetColor(color))
 				}
+				break FieldLoop
 			}
 		}
 	}
-	return csiParameter, style
+	return style
 }
 
 // strToContents converts a single-line string into a content array.
@@ -249,11 +247,16 @@ func strToContents(line string, tabWidth int) []content {
 				continue
 			}
 		case ansiControlSequence:
-			if runeValue >= 0x30 && runeValue <= 0x3f {
-				csiParameter.WriteRune(runeValue)
-				continue
+			if runeValue == 'm' {
+				style = csToStyle(csiParameter, runeValue)
+			} else if runeValue >= 'A' && runeValue <= 'T' {
+				// Ignore.
+			} else {
+				if runeValue >= 0x30 && runeValue <= 0x3f {
+					csiParameter.WriteRune(runeValue)
+					continue
+				}
 			}
-			csiParameter, style = csToStyle(csiParameter, runeValue)
 			state = ansiText
 			continue
 		}
