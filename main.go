f153bd26d049c78c3c49e8c00fee06273a0f6eab
Noboru Saito <noborusai@gmail.com>
2020-04-21 09:39:39 +0900

    internal -> internal/oviewer

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/gdamore/tcell"
 	homedir "github.com/mitchellh/go-homedir"
-	oviewer "github.com/noborus/oviewer/internal"
+	"github.com/noborus/oviewer/internal/oviewer"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
