df3c63fbfbe574dde6dd315f85a02b83c7f9c42e
Noboru Saito <noborusai@gmail.com>
2022-02-10 11:00:08 +0900

    Added benchmark test
    Add benchmark tests for parse and draw.
    This benchmark test actually requires a terminal.
    May be useful for investigating #144.

---
+++ oviewer/benchmark_test.go
@@ -0,0 +1,55 @@
+package oviewer
+
+import (
+	"os"
+	"testing"
+)
+
+func BenchmarkParseString_Normal(b *testing.B) {
+	f, err := os.ReadFile("../testdata/normal.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		parseString(string(f), 8)
+	}
+}
+
+func BenchmarkParseString_AnsiEscape(b *testing.B) {
+	f, err := os.ReadFile("../testdata/ansiescape.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		parseString(string(f), 8)
+	}
+}
+
+func BenchmarkDraw_Normal(b *testing.B) {
+	Draw_Helper(b, "../testdata/normal.txt")
+
+}
+
+func BenchmarkDraw_AnsiEscape(b *testing.B) {
+	Draw_Helper(b, "../testdata/ansiescape.txt")
+}
+
+func BenchmarkDraw_ChromaTerm(b *testing.B) {
+	Draw_Helper(b, "../testdata/ct.log")
+}
+
+func Draw_Helper(b *testing.B, fileName string) {
+	root, err := Open(fileName)
+	if err != nil {
+		b.Fatal(err)
+	}
+	root.ViewSync()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		root.draw()
+		root.Doc.ClearCache()
+	}
+	root.Close()
+}
