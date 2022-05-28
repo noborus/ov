f82f47ba5f8e2b7cdee40b607fc5b60830140b2e
Noboru Saito <noborusai@gmail.com>
2022-05-28 12:43:53 +0900

    Added unmarshal to the test
    Removed duplicates in yaml settings.

--- config_test.go
+++ config_test.go
@@ -3,8 +3,11 @@ package main
 import (
 	"testing"
 
+	"github.com/noborus/ov/oviewer"
 	"github.com/spf13/viper"
 )
+
+var tconfig oviewer.Config
 
 // This test is a yaml file check.
 func Test_Config(t *testing.T) {
@@ -12,38 +15,45 @@ func Test_Config(t *testing.T) {
 		cfgFile string
 	}
 	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
+		name     string
+		args     args
+		wantErr  bool
+		wantErr2 bool
 	}{
 		{
 			name: "test ov.yaml",
 			args: args{
 				cfgFile: "ov.yaml",
 			},
-			wantErr: false,
+			wantErr:  false,
+			wantErr2: false,
 		},
 		{
 			name: "test ov-less.yaml",
 			args: args{
 				cfgFile: "ov-less.yaml",
 			},
-			wantErr: false,
+			wantErr:  false,
+			wantErr2: false,
 		},
 		{
 			name: "test error",
 			args: args{
 				cfgFile: "config-fail.yaml",
 			},
-			wantErr: true,
+			wantErr:  true,
+			wantErr2: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			viper.SetConfigFile(tt.args.cfgFile)
-			err := viper.ReadInConfig()
-			if (err != nil) != tt.wantErr {
+			if err := viper.ReadInConfig(); (err != nil) != tt.wantErr {
 				t.Errorf("Config error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err := viper.Unmarshal(&tconfig); (err != nil) != tt.wantErr2 {
+				t.Errorf("Config error = %v, wantErr2 %v", err, tt.wantErr2)
 				return
 			}
 		})
