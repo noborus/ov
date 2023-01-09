7b7920b81f3bf9e846b76b9a047d68531f9dc92e
Noboru Saito <noborusai@gmail.com>
2023-01-09 10:18:19 +0900

    Fixed a bug where allIndex never finished
    Fixed a bug that didn't exit when substr was an empty string.

--- oviewer/utils_test.go
+++ oviewer/utils_test.go
@@ -262,6 +262,14 @@ func Test_allIndex(t *testing.T) {
 			},
 			want: nil,
 		},
+		{
+			name: "testNoSubstr",
+			args: args{
+				s:      "a,b,c",
+				substr: "",
+			},
+			want: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
