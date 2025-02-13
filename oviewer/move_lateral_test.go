18988aaafc4bd2df6426fae141a274ce69ab140e
Noboru Saito <noborusai@gmail.com>
2025-02-13 10:33:40 +0900

    Improve column cursor movement tests
    Enhanced the tests for column cursor movement.

--- oviewer/move_lateral_test.go
+++ oviewer/move_lateral_test.go
@@ -19,6 +19,8 @@ func TestDocument_optimalCursor(t *testing.T) {
 		columnDelimiter string
 		x               int
 		width           int
+		HeaderColumn    int
+		verticalHeader  int
 	}
 	type args struct {
 		cursor int
@@ -66,6 +68,89 @@ func TestDocument_optimalCursor(t *testing.T) {
 			},
 			want: 5,
 		},
+		{
+			name: "OverColumn",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				x:               10,
+				width:           40,
+			},
+			args: args{
+				cursor: 20,
+			},
+			want: 7,
+		},
+		{
+			name: "VerticalHeader1",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				x:               10,
+				width:           40,
+				verticalHeader:  5,
+			},
+			args: args{
+				cursor: 0,
+			},
+			want: 3,
+		},
+		{
+			name: "VerticalHeader2",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				x:               10,
+				width:           40,
+				verticalHeader:  20,
+			},
+			args: args{
+				cursor: 0,
+			},
+			want: 5,
+		},
+		{
+			name: "HeaderColumn1",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				HeaderColumn:    1,
+				x:               10,
+				width:           40,
+			},
+			args: args{
+				cursor: 3,
+			},
+			want: 3,
+		},
+		{
+			name: "HeaderColumn2",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				HeaderColumn:    4,
+				x:               10,
+				width:           30,
+			},
+			args: args{
+				cursor: 6,
+			},
+			want: 4,
+		},
+		{
+			name: "HeaderColumn3",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				HeaderColumn:    4,
+				x:               10,
+				width:           40,
+			},
+			args: args{
+				cursor: 7,
+			},
+			want: 5,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -77,6 +162,8 @@ func TestDocument_optimalCursor(t *testing.T) {
 			m.ColumnDelimiter = tt.fields.columnDelimiter
 			m.x = tt.fields.x
 			m.width = tt.fields.width
+			m.VerticalHeader = tt.fields.verticalHeader
+			m.HeaderColumn = tt.fields.HeaderColumn
 			ctx := context.Background()
 			root.prepareDraw(ctx)
 			if got := m.optimalCursor(root.scr, tt.args.cursor); got != tt.want {
@@ -86,267 +173,427 @@ func TestDocument_optimalCursor(t *testing.T) {
 	}
 }
 
-func TestRoot_moveColumnWithLeft(t *testing.T) {
+func TestDocument_optimalX(t *testing.T) {
 	tcellNewScreen = fakeScreen
 	defer func() {
 		tcellNewScreen = tcell.NewScreen
 	}()
 	type fields struct {
-		cursor int
+		fileName        string
+		wrap            bool
+		columnDelimiter string
+		x               int
+		width           int
+		HeaderColumn    int
+		verticalHeader  int
 	}
 	type args struct {
-		n     int
-		cycle bool
+		cursor int
 	}
 	tests := []struct {
-		name       string
-		fields     fields
-		args       args
-		wantErr    bool
-		wantCursor int
+		name    string
+		fields  fields
+		args    args
+		wantX   int
+		wantErr bool
 	}{
 		{
-			name: "ps1",
+			name: "cursorAtZero",
 			fields: fields{
-				cursor: 1,
+				fileName: filepath.Join(testdata, "normal.txt"),
+				width:    40,
 			},
 			args: args{
-				n:     1,
-				cycle: true,
+				cursor: 0,
 			},
-			wantErr:    false,
-			wantCursor: 0,
+			wantX:   0,
+			wantErr: false,
 		},
 		{
-			name: "ps2",
+			name: "cursorLessThanHeaderColumn",
 			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				width:           40,
+				HeaderColumn:    5,
+			},
+			args: args{
+				cursor: 3,
+			},
+			wantX:   0,
+			wantErr: false,
+		},
+		{
+			name: "cursorGreaterThanColumns",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				width:           40,
+				x:               10,
+			},
+			args: args{
+				cursor: 20,
+			},
+			wantX:   10,
+			wantErr: true,
+		},
+		{
+			name: "cursorWithinScreen",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				width:           40,
+				x:               10,
+			},
+			args: args{
 				cursor: 4,
 			},
-			args: args{
-				n:     2,
-				cycle: true,
-			},
-			wantErr:    false,
-			wantCursor: 2,
+			wantX:   10,
+			wantErr: false,
 		},
 		{
-			name: "psCycle",
+			name: "cursorOffScreen",
 			fields: fields{
-				cursor: 0,
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				columnDelimiter: ",",
+				width:           40,
+				x:               10,
 			},
 			args: args{
-				n:     1,
-				cycle: true,
+				cursor: 6,
 			},
-			wantErr:    false,
-			wantCursor: 10,
-		},
-		{
-			name: "psNoCycle",
-			fields: fields{
-				cursor: 0,
-			},
-			args: args{
-				n:     1,
-				cycle: false,
-			},
-			wantErr:    true,
-			wantCursor: 0,
+			wantX:   20,
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			root := rootFileReadHelper(t, filepath.Join(testdata, "ps.txt"))
+			root := rootFileReadHelper(t, tt.fields.fileName)
 			root.prepareScreen()
 			m := root.Doc
-			m.ColumnWidth = true
+			m.WrapMode = tt.fields.wrap
 			m.ColumnMode = true
-			m.columnCursor = tt.fields.cursor
-			root.prepareDraw(context.Background())
-			if err := m.moveColumnLeft(tt.args.n, root.scr, tt.args.cycle); (err != nil) != tt.wantErr {
-				t.Errorf("Document.moveColumnWidthLeft() error = %v, wantErr %v", err, tt.wantErr)
-			}
-			if got := m.columnCursor; got != tt.wantCursor {
-				t.Errorf("Document.moveColumnWidthLeft() = %v, want %v", got, tt.wantCursor)
-			}
-		})
-	}
-}
-
-func TestDocument_moveColumnWidthRight(t *testing.T) {
-	tcellNewScreen = fakeScreen
-	defer func() {
-		tcellNewScreen = tcell.NewScreen
-	}()
-	type fields struct {
-		cursor int
-		x      int
-	}
-	type args struct {
-		n     int
-		cycle bool
-	}
-	tests := []struct {
-		name       string
-		fields     fields
-		args       args
-		wantErr    bool
-		wantCursor int
-	}{
-		{
-			name: "ps1",
-			fields: fields{
-				cursor: 1,
-			},
-			args: args{
-				n:     1,
-				cycle: true,
-			},
-			wantErr:    false,
-			wantCursor: 2,
-		},
-		{
-			name: "ps2",
-			fields: fields{
-				cursor: 4,
-			},
-			args: args{
-				n:     2,
-				cycle: true,
-			},
-			wantErr:    false,
-			wantCursor: 6,
-		},
-		{
-			name: "psCycle",
-			fields: fields{
-				cursor: 10,
-				x:      180,
-			},
-			args: args{
-				n:     1,
-				cycle: true,
-			},
-			wantErr:    false,
-			wantCursor: 0,
-		},
-		{
-			name: "psNoCycle",
-			fields: fields{
-				cursor: 10,
-				x:      180,
-			},
-			args: args{
-				n:     1,
-				cycle: false,
-			},
-			wantErr:    true,
-			wantCursor: 10,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			root := rootFileReadHelper(t, filepath.Join(testdata, "ps.txt"))
-			root.prepareScreen()
-			m := root.Doc
-			m.ColumnMode = true
-			m.ColumnWidth = true
-			m.columnCursor = tt.fields.cursor
-			m.setColumnWidths()
+			m.ColumnDelimiter = tt.fields.columnDelimiter
 			m.x = tt.fields.x
-			root.prepareDraw(context.Background())
-			if err := m.moveColumnRight(tt.args.n, root.scr, tt.args.cycle); (err != nil) != tt.wantErr {
-				t.Errorf("Document.moveColumnWidthRight() error = %v, wantErr %v", err, tt.wantErr)
+			m.width = tt.fields.width
+			m.VerticalHeader = tt.fields.verticalHeader
+			m.HeaderColumn = tt.fields.HeaderColumn
+			ctx := context.Background()
+			root.prepareDraw(ctx)
+			gotX, err := m.optimalX(root.scr, tt.args.cursor)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Document.optimalX() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
-			if got := m.columnCursor; got != tt.wantCursor {
-				t.Errorf("Document.moveColumnWidthRight() = %v, want %v", got, tt.wantCursor)
+			if gotX != tt.wantX {
+				t.Errorf("Document.optimalX() = %v, want %v", gotX, tt.wantX)
 			}
 		})
 	}
 }
-
-func TestDocument_moveColumnDelimiterLeft(t *testing.T) {
+func TestDocument_moveColumnLeft(t *testing.T) {
 	tcellNewScreen = fakeScreen
 	defer func() {
 		tcellNewScreen = tcell.NewScreen
 	}()
 	type fields struct {
-		cursor int
+		fileName        string
+		wrap            bool
+		columnDelimiter string
+		x               int
+		width           int
+		HeaderColumn    int
+		verticalHeader  int
+		columnCursor    int
 	}
 	type args struct {
 		n     int
 		cycle bool
 	}
 	tests := []struct {
-		name       string
-		fields     fields
-		args       args
-		wantErr    bool
-		wantCursor int
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		wantX   int
 	}{
 		{
-			name: "csv1",
+			name: "moveLeftWithinBounds",
 			fields: fields{
-				cursor: 1,
-			},
-			args: args{
-				n:     1,
-				cycle: true,
-			},
-			wantErr:    false,
-			wantCursor: 0,
-		},
-		{
-			name: "csv2",
-			fields: fields{
-				cursor: 4,
-			},
-			args: args{
-				n:     1,
-				cycle: true,
-			},
-			wantErr:    false,
-			wantCursor: 3,
-		},
-		{
-			name: "csvCycle",
-			fields: fields{
-				cursor: 0,
-			},
-			args: args{
-				n:     1,
-				cycle: true,
-			},
-			wantErr:    false,
-			wantCursor: 7,
-		},
-		{
-			name: "csvNoCycle",
-			fields: fields{
-				cursor: 0,
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               20,
+				width:           40,
+				columnCursor:    3,
 			},
 			args: args{
 				n:     1,
 				cycle: false,
 			},
-			wantErr:    true,
-			wantCursor: 0,
+			wantErr: false,
+			wantX:   0,
+		},
+		{
+			name: "moveLeftOutOfBoundsNoCycle",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               0,
+				width:           40,
+				columnCursor:    0,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: true,
+			wantX:   0,
+		},
+		{
+			name: "moveLeftOutOfBoundsWithCycle",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               20,
+				width:           40,
+				columnCursor:    0,
+			},
+			args: args{
+				n:     1,
+				cycle: true,
+			},
+			wantErr: false,
+			wantX:   0,
+		},
+		{
+			name: "moveLeftToHeaderColumn",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               30,
+				width:           40,
+				columnCursor:    5,
+				HeaderColumn:    4,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: false,
+			wantX:   25,
+		},
+		{
+			name: "moveLeftWithWrapMode",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            true,
+				columnDelimiter: ",",
+				x:               20,
+				width:           40,
+				columnCursor:    3,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: false,
+			wantX:   0,
+		},
+		{
+			name: "moveLeftColumn2",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "column2.txt"),
+				wrap:            false,
+				columnDelimiter: "|",
+				x:               80,
+				width:           160,
+				columnCursor:    2,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: false,
+			wantX:   44,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			root := rootFileReadHelper(t, filepath.Join(testdata, "MOCK_DATA.csv"))
+			root := rootFileReadHelper(t, tt.fields.fileName)
 			root.prepareScreen()
 			m := root.Doc
+			m.WrapMode = tt.fields.wrap
 			m.ColumnMode = true
-			m.columnCursor = tt.fields.cursor
-			m.setDelimiter(",")
-			root.prepareDraw(context.Background())
+			m.ColumnDelimiter = tt.fields.columnDelimiter
+			m.x = tt.fields.x
+			m.width = tt.fields.width
+			m.VerticalHeader = tt.fields.verticalHeader
+			m.HeaderColumn = tt.fields.HeaderColumn
+			m.columnCursor = tt.fields.columnCursor
+			ctx := context.Background()
+			root.prepareDraw(ctx)
 			if err := m.moveColumnLeft(tt.args.n, root.scr, tt.args.cycle); (err != nil) != tt.wantErr {
-				t.Errorf("Document.moveColumnDelimiterLeft() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("Document.moveColumnLeft() error = %v, wantErr %v", err, tt.wantErr)
 			}
-			if got := m.columnCursor; got != tt.wantCursor {
-				t.Errorf("Document.moveColumnDelimiterLeft() = %v, want %v", got, tt.wantCursor)
+			if m.x != tt.wantX {
+				t.Errorf("Document.moveColumnLeft() = %v, want %v", m.x, tt.wantX)
+			}
+		})
+	}
+}
+func TestDocument_moveColumnRight(t *testing.T) {
+	tcellNewScreen = fakeScreen
+	defer func() {
+		tcellNewScreen = tcell.NewScreen
+	}()
+	type fields struct {
+		fileName        string
+		wrap            bool
+		columnDelimiter string
+		x               int
+		width           int
+		HeaderColumn    int
+		verticalHeader  int
+		columnCursor    int
+	}
+	type args struct {
+		n     int
+		cycle bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		wantX   int
+	}{
+		{
+			name: "moveRightWithinBounds",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               10,
+				width:           40,
+				columnCursor:    2,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: false,
+			wantX:   10,
+		},
+		{
+			name: "moveRightOutOfBoundsNoCycle",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               39,
+				width:           40,
+				columnCursor:    8,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: true,
+			wantX:   39,
+		},
+		{
+			name: "moveRightOutOfBoundsWithCycle",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               39,
+				width:           40,
+				columnCursor:    8,
+			},
+			args: args{
+				n:     1,
+				cycle: true,
+			},
+			wantErr: false,
+			wantX:   0,
+		},
+		{
+			name: "moveRightToEndOfLine",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            false,
+				columnDelimiter: ",",
+				x:               7,
+				width:           40,
+				columnCursor:    7,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: false,
+			wantX:   9,
+		},
+		{
+			name: "moveRightWithWrapMode",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "MOCK_DATA.csv"),
+				wrap:            true,
+				columnDelimiter: ",",
+				x:               0,
+				width:           40,
+				columnCursor:    2,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: false,
+			wantX:   0,
+		},
+		{
+			name: "moveRightToHeaderColumn2",
+			fields: fields{
+				fileName:        filepath.Join(testdata, "column2.txt"),
+				wrap:            false,
+				columnDelimiter: "|",
+				x:               0,
+				width:           40,
+				columnCursor:    0,
+			},
+			args: args{
+				n:     1,
+				cycle: false,
+			},
+			wantErr: false,
+			wantX:   37,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			root := rootFileReadHelper(t, tt.fields.fileName)
+			root.prepareScreen()
+			m := root.Doc
+			m.WrapMode = tt.fields.wrap
+			m.ColumnMode = true
+			m.ColumnDelimiter = tt.fields.columnDelimiter
+			m.x = tt.fields.x
+			m.width = tt.fields.width
+			m.VerticalHeader = tt.fields.verticalHeader
+			m.HeaderColumn = tt.fields.HeaderColumn
+			m.columnCursor = tt.fields.columnCursor
+			ctx := context.Background()
+			root.prepareDraw(ctx)
+			if err := m.moveColumnRight(tt.args.n, root.scr, tt.args.cycle); (err != nil) != tt.wantErr {
+				t.Errorf("Document.moveColumnRight() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if m.x != tt.wantX {
+				t.Errorf("Document.moveColumnRight() = %v, want %v", m.x, tt.wantX)
 			}
 		})
 	}
