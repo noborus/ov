48e02b50354102d7952190e97643dee04add4958
Noboru Saito <noborusai@gmail.com>
2025-03-06 12:23:14 +0900

    Fix prompt message test and message

--- oviewer/input_header_column.go
+++ oviewer/input_header_column.go
@@ -31,7 +31,7 @@ func (*eventHeaderColumn) Mode() InputMode {
 
 // Prompt returns the prompt string in the input field.
 func (*eventHeaderColumn) Prompt() string {
-	return "Header Column:"
+	return "Header column:"
 }
 
 // Confirm returns the event when the input is confirmed.
