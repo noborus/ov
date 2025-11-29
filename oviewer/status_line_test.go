9fdc82275013568ae507637776aba394f04bb10b
Noboru Saito <noborusai@gmail.com>
2025-11-29 13:19:56 +0900

    Change the status so that it does not use SetContent
    improve status line rendering by using PutStrStyled instead of
    SetContent for each character.

--- oviewer/status_line_test.go
+++ oviewer/status_line_test.go
@@ -1,95 +1,10 @@
 package oviewer
 
 import (
-	"path/filepath"
 	"testing"
-
-	"github.com/gdamore/tcell/v2"
 )
 
-func TestRoot_leftStatus(t *testing.T) {
-	type fields struct {
-		eventer   Eventer
-		caption   string
-		inputMode InputMode
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-		{
-			name: "test normal",
-			fields: fields{
-				caption:   "test",
-				eventer:   normal(),
-				inputMode: Normal,
-			},
-			want: "test:",
-		},
-		{
-			name: "test search prompt",
-			fields: fields{
-				caption:   "test",
-				eventer:   newSearchEvent(blankCandidate(), forward),
-				inputMode: Search,
-			},
-			want: "/",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			root := rootHelper(t)
-			root.Doc.Caption = tt.fields.caption
-			root.input.Event = tt.fields.eventer
-			got, _ := root.leftStatus()
-			gotStr := got.String()
-			if gotStr != tt.want {
-				t.Errorf("Root.leftStatus() got = %v, want %v", gotStr, tt.want)
-			}
-		})
-	}
-}
-
-func TestRoot_leftStatus2(t *testing.T) {
-	tcellNewScreen = fakeScreen
-	defer func() {
-		tcellNewScreen = tcell.NewScreen
-	}()
-	type fields struct {
-		fileNames []string
-		eventer   Eventer
-		inputMode InputMode
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-		{
-			name: "test normal",
-			fields: fields{
-				fileNames: []string{filepath.Join(testdata, "test.txt"), filepath.Join(testdata, "section2.txt")},
-				eventer:   normal(),
-				inputMode: Normal,
-			},
-			want: "[0]" + filepath.Join(testdata, "test.txt") + ":",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			root := rootFileReadHelper(t, tt.fields.fileNames...)
-			root.input.Event = tt.fields.eventer
-			got, _ := root.leftStatus()
-			gotStr := got.String()
-			if gotStr != tt.want {
-				t.Errorf("Root.leftStatus() got = %v, want %v", gotStr, tt.want)
-			}
-		})
-	}
-}
-
-func TestRoot_statusDisplay(t *testing.T) {
+func TestRoot_statusMode(t *testing.T) {
 	type fields struct {
 		FollowSection bool
 		FollowAll     bool
@@ -139,14 +54,6 @@ func TestRoot_statusDisplay(t *testing.T) {
 			},
 			want: "(Follow Mode)",
 		},
-		{
-			name: "pause follow",
-			fields: fields{
-				FollowMode:  true,
-				pauseFollow: true,
-			},
-			want: "||(Follow Mode)",
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -157,7 +64,7 @@ func TestRoot_statusDisplay(t *testing.T) {
 			root.Doc.FollowName = tt.fields.FollowName
 			root.Doc.FollowMode = tt.fields.FollowMode
 			root.Doc.pauseFollow = tt.fields.pauseFollow
-			if got := root.displayStatus(); got != tt.want {
+			if got := root.statusMode(); got != tt.want {
 				t.Errorf("Root.statusDisplay() = %v, want %v", got, tt.want)
 			}
 		})
